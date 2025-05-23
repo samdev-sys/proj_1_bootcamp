c6e30bc2d79f45bba548bef2adb525df97e49d9f
samdev-sys <sv4432@gmail.com>
2025-05-23 03:00:46 +0000

    update

--- Backend/login.js
+++ Backend/login.js
@@ -1,17 +1,3 @@
-document.getElementById('loginForm').addEventListener('submit', async (event) => {
-    event.preventDefault();
-    const username = document.getElementById('usernameLog').value;
-    const password = document.getElementById('passwordLog').value;
-    
-    const response = await fetch('/login', {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ username, password })
-    });
-
-    const result = await response.json();
-    alert(result.message);
-});
 
 async function genPass() {
     const response = await fetch("http://localhost:3000/generar-contraseña");
--- Backend/script.js
+++ Backend/script.js
@@ -1,2 +1,13 @@
+function iniciarSesion(){
+    let user =document.getElementById('usernameLog').value;
+    let password=document.getElementById('passwordLog').value;
+    let inicio=document.getElementById('btn_inicioSesion');
 
-  
+    if (password===pass){
+        
+        alert('inicio de sesion correcto');
+    }
+    else{
+        alert('intente nuevamente');
+    }
+}
