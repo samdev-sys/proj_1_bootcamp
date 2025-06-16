15ec4aa0ce857b5ebdc6171aa7d64066e129835b
samdev-sys <sv4432@gmail.com>
2025-06-15 21:06:07 -0500

    actualiza backend

--- Backend/server.js
+++ Backend/server.js
@@ -1,3 +1,15 @@
 const express = require('express');
 const cors =require('cors');
-const db=require('./db')
+const db=require('./db');
+const bodyParser = require('body-parser');
+const app= express();
+const PORT =3307;
+
+
+app.use (cors());
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+
+app.post('/')
