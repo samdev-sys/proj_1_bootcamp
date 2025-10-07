d0a695c51fe9921d04075ca17aa08053cbd7928d
samdev-sys <sv4432@gmail.com>
2025-10-06 22:41:35 -0500

    replace_db_hosting

--- ConectBD/conexion_MySQL.js
+++ ConectBD/conexion_MySQL.js
@@ -8,7 +8,7 @@ const db = mysql.createConnection({
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
   ssl:{
-    ca: fs.readFileSync("C:/Users/user/Desktop/proj_1_bootcamp/ca.pem"),
+    ca: fs.readFileSync("C:/Users/user/Desktop/proj_1_bootcamp/ConectBD/ca.pem"),
     rejectUnauthorized: true
   }
 });
