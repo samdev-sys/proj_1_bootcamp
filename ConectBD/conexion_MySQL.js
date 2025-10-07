9c19492dc4615a0edd96460cf280f0ef4020ae79
samdev-sys <sv4432@gmail.com>
2025-10-06 22:28:37 -0500

    replace_db_hosting

--- ConectBD/conexion_MySQL.js
+++ ConectBD/conexion_MySQL.js
@@ -6,13 +6,17 @@ const db = mysql.createConnection({
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
   database: process.env.MYSQLDATABASE,
+  ssl:{
+    ca: fs.readFileSync("C:/Users/user/Desktop/proj_1_bootcamp/ca.pem"),
+    rejectUnauthorized: true
+  }
 });
 
 db.connect(err => {
   if (err) {
     console.error('❌ Error de conexión:', err);
   } else {
-    console.log('✅ Conectado a MySQL en Railway');
+    console.log('✅ Conectado a MySQL en aiven');
   }
 });
 console.log('📦 Base de datos seleccionada:', process.env.MYSQLDATABASE);
